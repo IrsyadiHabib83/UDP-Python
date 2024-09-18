2c9157023e38f32f1fd9d44ec564ab32b2cec7bd
Irsyadi habib <139208955+IrsyadiHabib83@users.noreply.github.com>
2024-09-18 10:38:48 +0700

    Update UDP_Client.py

--- UDP_Client.py
+++ UDP_Client.py
@@ -1,18 +1,15 @@
 import socket
 
 # Membuat socket UDP
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Alamat IP server dan port
-server_address = ('127.0.0.1', 12345)
+
 
 # Mengirim pesan ke server
-message = "Halo server!"
-client_socket.sendto(message.encode(), server_address)
+
 
 # Menerima balasan dari server
-data, addr = client_socket.recvfrom(1024)
-print(f"Balasan dari server: {data.decode()}")
 
 
-client_socket.close()
+
+#Tutup koneksi
